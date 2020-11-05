823acf162091713d0566751609cb7c324e7d2b97
Paul Park <tue63405@temple.edu>
2020-11-04 23:31:28 -0500

    css updates

--- src/components/Nav.js
+++ src/components/Nav.js
@@ -2,6 +2,33 @@ import React from 'react'
 import {Link} from 'react-router-dom'
 
 function Nav() {
+    if (window.location.href === 'http://localhost:3000/') {
+        return (
+            <nav className='nav-bar'>
+          <h3 style={{ fontWeight: 'bold', color: '#ffffff'}}>Amani Clothing Co Logo</h3>
+            <ul className='nav-list'>
+                <Link to='/' style={{ fontWeight: 'bold', textDecoration: 'none', color: '#ffffff' }}>
+                    <li className='nav-link'>Home</li>
+                </Link>
+                <Link to='/about' style={{ fontWeight: 'bold', textDecoration: 'none', color: '#ffffff' }}>
+                    <li className='nav-link'>About Us</li>
+                </Link>
+                <Link to='/shop' style={{ fontWeight: 'bold', textDecoration: 'none', color: '#ffffff' }}>
+                    <li className='nav-link'>Shop</li>
+                </Link>
+                <Link to='/ambassador'style={{ fontWeight: 'bold', textDecoration: 'none', color: '#ffffff' }}>
+                    <li className='nav-link'>Ambassador</li>
+                </Link>
+                <Link to='/faq'style={{ fontWeight: 'bold', textDecoration: 'none', color: '#ffffff' }}>
+                    <li className='nav-link'>FAQ</li>
+                </Link>
+                <Link to='/cart'style={{ fontWeight: 'bold', textDecoration: 'none', color: '#ffffff' }}>
+                    <li className='nav-link'>Cart</li>
+                </Link>
+            </ul>
+        </nav>
+        )
+    } else {
     return (
         <nav className='nav-bar'>
           <h3>Amani Clothing Co Logo</h3>
@@ -27,6 +54,7 @@ function Nav() {
             </ul>
         </nav>
     )
+}
 }
 
 export default Nav
